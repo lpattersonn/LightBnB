97708db934f45b91727a0f68e04eff6b3d3d8e39
Leshan Patterson <leshan0627@gmail.com>
2022-01-08 15:56:31 +0000

    fix: corrected porperties not showing by fixing queries in db.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -12,29 +12,22 @@ const users = require("./json/users.json");
 
 /// Users
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
 const getUserWithEmail = function (email) {
-  return pool
-    .query(
-      `
-  SELECT * FROM users
-  WHERE users.email AND
-  email=$1
-`,
-      [email]
-    )
-    .then((result) => {
-      result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message, NULL);
-    });
+  return pool.query(`SELECT * FROM users WHERE users.email LIKE $1`, [email])
+  .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message, NULL));
 };
 exports.getUserWithEmail = getUserWithEmail;
+
+// const getUserWithEmail = function (email) {
+// return pool
+//   .query(`SELECT * FROM users WHERE users.email LIKE $1`, [email])
+//   .then((result) => result.rows[0])
+//   .catch((err) => {
+//     console.log(err.message)
+//   });
+// };
+// exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
@@ -47,13 +40,12 @@ const getUserWithId = (id) => {
       `
   SELECT * 
   FROM users
-  WHERE users.id
-  id=$1
+  WHERE users.id=$1
   `,
       [id]
     )
-    .then((result) => result.rows)
-    .catch((err) => console.log(err.message, NULL));
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
 };
 exports.getUserWithId = getUserWithId;
 
@@ -62,53 +54,53 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
 const addUser = function (user) {
-  return pool
-    .query(
-      `INSERT INTO users (name, email, password)
-  VALUES ($1, $2, $3) RETURNING *`,
-      [user.name, user.email, user.password]
-    )
-    .then((result) => {
-      result;
-    })
-    .catch((error) => {
-      error.message;
-    });
+  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [user.name, user.email, user.password])
+  .then((result) => result.rows[0])
+  .catch((err) => {
+    console.log(err.message)
+  });
 };
+
 exports.addUser = addUser;
+
 
 /// Reservations
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return pool
+//     .query(
+//       `
+//   SELECT *
+//   FROM reservations
+//   JOIN properties ON properties.id=property_id
+//   WHERE guest_id = $1
+//   LIMIT = $2
+//   `,
+//       [guest_id, limit]
+//     )
+//     .then((reuslt) =);
+// };
+// exports.getAllReservations = getAllReservations;
+
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
-    .query(
-      `
-  SELECT properties.*
-  FROM reservations
-  JOIN properties ON properties.id=property_id
-  WHERE guest_id = $1
-  LIMIT = $2
-  `,
-      [guest_id, limit]
-    )
-    .then(reuslt);
+  return pool.query(`
+    SELECT *
+    FROM reservations
+    JOIN properties ON properties.id = property_id
+    WHERE guest_id = $1
+    LIMIT $2
+    `, [guest_id, limit])
+  .then((result) => result.rows)
+  .catch((err) => {
+    console.log(err.message)
+  });
 };
 exports.getAllReservations = getAllReservations;
 
 /// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
 const getAllProperties = function (options, limit = 10) {
   // 1
   const queryParams = [];
@@ -118,29 +110,50 @@ const getAllProperties = function (options, limit = 10) {
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   `;
-
-  // 3
+  if (options.city && options.owner_id) {
+    const owner_identifier = Number(options.owner_id)
+    queryParams.push(`%${owner_identifier}%`);
+    queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+    console.log(options)
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += ` AND city LIKE $${queryParams.length}`;
+    console.log(options)
   }
-  // if an owner_id is passed in, only return properties belonging to that owner.
-  if (options.owner_id) {
-    queryParams.push(`%${otions.owner_id}`);
-    queryString += `AND WHERE owner_id LIKE $${queryParams.length}`;
-  }
-  //if a minimum_price_per_night and a maximum_price_per_night, only return properties within that price range.
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
-    queryParams.push(`%${options.minimum_price_per_night}`);
-    queryString += `AND WHERE minimum_price_per_night LIKE $${queryParams.length}`;
-    queryParams.push(`%${options.maximum_price_per_night}`);
-    queryString += `AND WHERE options.maximum_price_per_night LIKE $${queryParams.length}`;
+      queryParams.push(`%${options.minimum_price_per_night}%`);
+      queryString += ` AND minimum_price_per_night LIKE $${queryParams.length}`;
+      queryParams.push(`%${options.maximum_price_per_night}%`);
+      queryString += ` AND options.maximum_price_per_night LIKE $${queryParams.length}`;
+      console.log(options.maximum_price_per_night, options.minimum_price_per_night)
   }
-  // if a minimum_rating is passed in, only return properties with a rating equal to or higher than that.
-  if (options.minimum_rating) {
-    queryParams.push(`%${options.minimum_rating}`);
-    queryString += `AND WHERE minimum_rating >= $${queryParams.length}`;
-  }
+}
+  // if (options.city === '' && options.owner_id) {
+  //   queryParams.push(`%${options.owner_id}%`);
+  //   queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+  //   console.log(options)
+  // } else if (options.city !== '' && options.owner_id) {
+  //   queryParams.push(`%${options.owner_id}%`);
+  //   queryString += ` AND owner_id LIKE $${queryParams.length}`;
+  //   console.log(options)
+  // }
+  // if an owner_id is passed in, only return properties belonging to that owner.
+  // if (options.owner_id) {
+  //   queryParams.push(`%${otions.owner_id}`);
+  //   queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+  // }
+  //if a minimum_price_per_night and a maximum_price_per_night, only return properties within that price range.
+  // if (options.minimum_price_per_night && options.maximum_price_per_night) {
+  //   queryParams.push(`%${options.minimum_price_per_night}`);
+  //   queryString += `WHERE minimum_price_per_night LIKE $${queryParams.length}`;
+  //   queryParams.push(`%${options.maximum_price_per_night}`);
+  //   queryString += `WHERE options.maximum_price_per_night LIKE $${queryParams.length}`;
+  // }
+  // // if a minimum_rating is passed in, only return properties with a rating equal to or higher than that.
+  // if (options.minimum_rating) {
+  //   queryParams.push(`%${options.minimum_rating}`);
+  //   queryString += ` AND WHERE minimum_rating >= $${queryParams.length}`;
+  // }
   // 4
   queryParams.push(limit);
   queryString += `
@@ -176,8 +189,7 @@ const addProperty = function (property) {
     .query(
       `
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
-  RETURNING *;`,
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`,
       [
         property.owner_id,
         property.title,
@@ -196,7 +208,8 @@ const addProperty = function (property) {
       ]
     )
     .then((result) => {
-      result.row;
+      result.rows;
+      console.log(result.rows)
     })
     .catch((err) => {
       console.log(err);
